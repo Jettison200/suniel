fa901908eba97e7ed54f48d6bbd9362c21d0fcc8
Jettison200 <jettison200@gmail.com>
2020-09-19 23:04:41 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import HomeContainer from "./HomeContainer"
+import './App.css'
+function App() {
+  return (
+    <div className="app">
+       <HomeContainer/>
+    </div>
+  )
+}
+
+export default App
